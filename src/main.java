62f2c7bf500e4d826ab459d629317db03063b3a0
Иван Осокин <osokin_ivan1992@mail.ru>
2022-07-07 20:44:43 +0300

    initial commit

---
+++ src/main.java
@@ -0,0 +1,19 @@
+public class main {
+
+    public static void main(String[] args) {
+
+       int a = 200;
+       int b =1500;
+       if (b > 1000) {
+
+           System.out.println(b / 100 + b + a);
+       }else{
+           System.out.println( b + a);
+       }
+
+           
+
+                   
+       }
+           
+}
